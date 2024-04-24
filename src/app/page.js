d830be9d3f34a688716bcde87ab16f6ef6b8a1c1
Weijing Jay lin <weijingjaylin@gmail.com>
2024-04-24 02:43:27 -0700

    layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,92 +3,147 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div>
+        <div className={styles.description}>
+          <div>
+            <a
+              href="https://english.qixingaupair.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/11894021_1554793991.png"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={284}
+                height={123}
+                priority
+              />
+            </a>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <div className={styles.grid}>
+          <div className={styles.card} style={{ margin: 0 }}>
+            <div className="ev-module-text ev-module-edit-box" id="Mo_99ttX">
+              <p className={styles.evTextArticle2} style={{ marginTop: 40 }}>
+                <strong>QiXing AuPair</strong> offers a variety of programs for
+                foreign young people who are hoping to come to China and
+                experience Chinese culture/language as an Au Pair or English
+                teacher for internship. We also provide these programs to
+                Chinese students who want to go abroad to experience a different
+                culture and lifestyle.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <iframe
+              frameborder="0"
+              src="https://v.qq.com/txp/iframe/player.html?vid=q09259fybz7"
+              width="529"
+              height="356"
+              allowfullscreen="true"
+              style={{ maxWidth: "30ch" }}
+            ></iframe>
+          </div>
+        </div>
       </div>
+      <div className={styles.main}>
+        <div className={styles.grid}>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              About <span>-&gt;</span>
+            </h2>
+            <p>
+              QiXing Au Pair is an international agency based in Shenzhen,
+              China, dedicated to promoting cultural exchange between China and
+              the rest of the world.{" "}
+            </p>
+          </a>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Address <span>-&gt;</span>
+            </h2>
+            <div
+              className="ev-module-text ev-module-edit-box"
+              data-animate-name="fadeInUp"
+              data-animate-duration="1.00s"
+              data-animate-delay="0.10s"
+              id="Mo_SMuj95"
+              data-loadanimate="1"
+            >
+              <span>Room 202, Shangmei Science and Technology Building</span>
+              <span>No. 2009 Shahexi Road</span>
+              <div>Nanshan District</div>
+              <div>Shenzhen</div>
+              <div>Guangdong Province</div>
+              <div>China</div>
+            </div>
+          </a>
+        </div>
+        <div className={styles.grid}>
+          <a
+            href="https://english.qixingaupair.com/index.php?s=/index/about/fid/125/eid/1168.html#id"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Phone Call <span>-&gt;</span>
+            </h2>
+            <p>
+              <span>
+                <a
+                  href="https://english.qixingaupair.com/index.php?s=/index/about/fid/125/eid/1168.html#id"
+                  data-ke-src="https://english.qixingaupair.com/index.php?s=/index/about/fid/125/eid/1168.html#id"
+                >
+                  {" "}
+                </a>{" "}
+                <span>+ 86 181 2636 0492&nbsp;</span>+ 86 153 3889 5076
+                <a
+                  href="https://english.qixingaupair.com/index.php?s=/index/about/fid/125/eid/1168.html#id"
+                  data-ke-src="https://english.qixingaupair.com/index.php?s=/index/about/fid/125/eid/1168.html#id"
+                >
+                  &nbsp;
+                </a>
+                <div>+ 86 181 2636 4051</div>
+                <div>Bejing Time: 9am - 6pm</div>
+              </span>
+            </p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <a
+            href="https://english.qixingaupair.com"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Email <span>-&gt;</span>
+            </h2>
+            <span>
+              <span>
+                <div>qixingaupairdg@163.com</div>
+                <div>Qixingaupair2022@outlook.com</div>
+                <div>qixingchen2022@outlook.com</div>
+              </span>
+            </span>
+          </a>
+        </div>
       </div>
     </main>
   );
